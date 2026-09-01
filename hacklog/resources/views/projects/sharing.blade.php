004eeced4e6fb284c4f82f6100613bb139f7fae7
JayKitt28 <jaykitt19@gmail.com>
2026-09-01 16:05:35 -0400

    Remove double succes on team and sharing page

--- hacklog/resources/views/projects/sharing.blade.php
+++ hacklog/resources/views/projects/sharing.blade.php
@@ -12,13 +12,6 @@
     </div>
 </div>
 
-@if(session('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
-    </div>
-@endif
-
 @if(session('info'))
     <div class="alert alert-info alert-dismissible fade show" role="alert">
         {{ session('info') }}
